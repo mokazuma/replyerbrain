b519d56163e6f8fc2c5dac0d02cd0f9ab339f41c
Kazuma Mori <deracine83.54@gmail.com>
2022-01-11 09:35:10 -0500

    Add files via upload

---
+++ decoding.py
@@ -0,0 +1,91 @@
+
+import glob
+import numpy as np
+import pandas as pd
+from scipy import stats
+from sklearn.model_selection import KFold, GridSearchCV, cross_val_predict, permutation_test_score
+from sklearn.linear_model import Ridge
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import make_scorer, mean_absolute_error
+import nibabel as nib
+from nilearn.datasets import load_mni152_template
+from nilearn.image import resample_to_img
+
+def mlpipe(features, target, cv=10, rand=1, permnumber=None):
+
+    ### make pipeline
+    kf = KFold(n_splits=cv, shuffle=True, random_state=rand)
+    pipe = Pipeline([("preprocess", StandardScaler()), ("task", Ridge(random_state=rand))])
+    search_space = [{"task__alpha": np.logspace(-3, 3, 7)}]
+    clf = GridSearchCV(pipe, search_space, cv=cv, verbose=0, n_jobs=-1)
+
+    if permnumber is None:
+        ### get prediction result
+        cvres = cross_val_predict(clf, features, target, cv=kf, n_jobs=-1)
+
+        return pd.DataFrame({'true': target, 'pred': cvres})
+    else:
+        ### non-parametric test
+        def cor_loss_func(y_true, y_pred):
+            r, p = stats.pearsonr(y_true, y_pred)
+            return r
+        scoretype = make_scorer(cor_loss_func, greater_is_better=True)
+        score, perm_scores, pvalue = permutation_test_score(
+            clf, features, target, scoring=scoretype, cv=kf, n_permutations=int(permnumber),
+            verbose=1, n_jobs=-1)
+
+        return pd.DataFrame({'score': perm_scores})
+
+########################################################################
+# set data
+########################################################################
+template = load_mni152_template(resolution=2)
+niinorm = lambda x: resample_to_img(nib.load(x), template).get_fdata()
+
+### from CONN
+maskdat = niinorm('result/Reply_rev/seed_lIFG_vox200/ROI.ROIS.nii')
+filelist = sorted(glob.glob('result/firstlevel/BETA_Subject*_Condition001_Source340.nii'))
+
+### get feature
+feature = pd.DataFrame()
+for f in filelist:
+    ### whole brain correlation
+    dat3d = niinorm(f)
+
+    ### get masked correlation
+    bin3d = np.where(maskdat == 0, 0, dat3d)
+    seedcor = bin3d[np.nonzero(bin3d)]
+    # seedcor.shape
+
+    feature = pd.concat([feature, pd.DataFrame(seedcor).T])
+feature.to_pickle('data/cormat_seed_rev.pkl')
+
+########################################################################
+# brain prediction
+########################################################################
+##### set data
+feature = pd.read_pickle('data/cormat_seed_rev.pkl').reset_index(drop=True)
+snsdat = pd.read_csv('data/twicl223-222.csv').iloc[:222].reset_index(drop=True)
+target = snsdat['Reply_lognetwork']
+
+##### machine learning prediction
+cv = 2
+pred = pd.DataFrame(); stat = pd.DataFrame()
+for rr in range(100):
+    res = mlpipe(feature, target, cv=cv, rand=rr)
+    pred = pd.concat([pred, res.assign(id=range(len(feature)), rand=rr)])
+    r, p = stats.pearsonr(res['true'], res['pred'])
+    mae = mean_absolute_error(res['true'], res['pred'])
+    stat = pd.concat([stat, pd.DataFrame({'r': [r], 'mae': [mae]})])
+
+pred.to_pickle('result/decoding/pred.pkl')
+stat.to_pickle('result/decoding/stat.pkl')
+
+##### permutation test
+n = 10000
+perm = mlpipe(feature, target, cv=cv, permnumber=n)
+average = stat['r'].mean()
+len(perm.query('score > @average')) / len(perm)  # p-value
+
+perm.to_pickle('result/decoding/perm.pkl')
